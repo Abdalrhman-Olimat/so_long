157e8d29fb47123cf6ed7583df5a221be679617f
Abdalrhman_Olimat <abdalrhmanolimat911@gmaail.com>
2024-12-18 08:29:18 +0300

    fix main

--- src/test.c
+++ src/test.c
@@ -6,7 +6,7 @@
 /*   By: aeleimat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 15:44:56 by aeleimat          #+#    #+#             */
-/*   Updated: 2024/12/18 08:01:07 by aeleimat         ###   ########.fr       */
+/*   Updated: 2024/12/18 08:27:56 by aeleimat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,24 @@
 
 
 
-void error_handel(char *av)
+void error_handel(char *av, t_map data)
 {
+    int i;
+    
 	cheack_name(av);
 	cheack_empty(av);
 	map_rectangular(av);
 	map_wall_main(av);
 	count_e_p_c(av);
-	
+	if (!load_map(av, &data))
+    {
+        write(2, "Error loading map\n", 18);
+        exit (1);
+    }
+    i = check_path(data.map, data.height, data.width, data.player_x, data.player_y, data.collectibles);
+    free_map(data.map, data.height);
+    if (i == 0)
+        exit (1);
 }
 
 
@@ -34,17 +44,8 @@ int main(int ac, char **av)
         write(2, "Error1\n", 7);
         return (0);
     }
-    error_handel(av[1]);
-
-    if (!load_map(av[1], &data))
-    {
-        write(2, "Error loading map\n", 18);
-        return (1);
-    }
-
-    if (check_path(data.map, data.height, data.width, data.player_x, data.player_y, data.collectibles))
-        printf("Path is good\n");
-
-    free_map(data.map, data.height);
+    error_handel(av[1], data);
+    printf("abd\n");
+    
     return (0);
 }
