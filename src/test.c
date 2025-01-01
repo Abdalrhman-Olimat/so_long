90a593d513d653b8d178e3929f22df8274075da5
Abdalrhman_Olimat <abdalrhmanolimat911@gmaail.com>
2025-01-01 19:52:03 +0300

    fix 4 f

--- src/test.c
+++ src/test.c
@@ -6,7 +6,7 @@
 /*   By: aeleimat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 15:44:56 by aeleimat          #+#    #+#             */
-/*   Updated: 2025/01/01 11:35:29 by aeleimat         ###   ########.fr       */
+/*   Updated: 2025/01/01 12:30:32 by aeleimat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,7 @@ void cleanup_game(t_game *game)
 
 
 
+/*
 int load_map2(char *filename, t_game *game)
 {
     int	fd;
@@ -79,11 +80,14 @@ int load_map2(char *filename, t_game *game)
 	fd = open(filename, O_RDONLY);
     if (fd < 0)
         return (0);
+
+    game->map.height = 0;
+    game->map.width = 0;
+    game->map.map = NULL;
+
     while (1)
     {
-        char *line;
-        
-        line = get_next_line(fd);
+        char *line = get_next_line(fd);
         if (!line)
             break;
 
@@ -97,7 +101,6 @@ int load_map2(char *filename, t_game *game)
         // Track max width
         if (len > game->map.width)
             game->map.width = len;
-
         // Append new line to the map array
         char **tmp = malloc(sizeof(char *) * (game->map.height + 2));
         for (int i = 0; i < game->map.height; i++)
@@ -143,6 +146,107 @@ int load_map2(char *filename, t_game *game)
     }
     return (1);
 }
+*/
+void enemy_pos(t_game *game, int x, int y)
+{
+    game->enemies[game->num_enemies].x = x;
+    game->enemies[game->num_enemies].y = y;
+    game->num_enemies++;
+}
+
+void player_pos(t_game *game, int x, int y)
+{
+    game->map.player_x = x;
+    game->map.player_y = y;
+}
+
+int scan_map_for_obj(t_game *game)
+{
+    int y;
+    
+    y = 0;
+    while (y < game->map.height)
+    {
+        int x;
+
+        x = 0;
+        while (x < game->map.width)
+        {
+            char c;
+
+            c = game->map.map[y][x];
+            if (c == 'P')
+                player_pos(game, x, y);
+            else if (c == 'C')
+                game->map.collectibles++;
+            else if (c == 'D' && game->num_enemies < MAX_ENEMIES)
+                enemy_pos(game, x, y);
+            x++;
+        }
+        y++;
+    }
+    return (1);
+}
+
+int load_map2(char *filename, t_game *game)
+{
+    int fd;
+
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+        return (0);
+
+    // Read the first line to set the width
+    char *line = get_next_line(fd);
+    if (line)
+    {
+        int len = ft_strlen2(line);
+        if (len > 0 && line[len - 1] == '\n')
+        {
+            line[len - 1] = '\0';
+            len--;
+        }
+        game->map.width = len; // Set the width based on the first line
+    }
+
+    // Read the rest of the lines
+    while (line)
+    {
+        // Append new line to the map array
+        char **tmp = malloc(sizeof(char *) * (game->map.height + 2));
+        int i = 0;
+        while (i < game->map.height)
+        {
+            tmp[i] = game->map.map[i];
+            i++;
+        }
+        tmp[game->map.height] = line;
+        tmp[game->map.height + 1] = NULL;
+        free(game->map.map);
+        game->map.map = tmp;
+        game->map.height++;
+
+        // Read the next line
+        line = get_next_line(fd);
+        if (line)
+        {
+            int len = ft_strlen2(line);
+            if (len > 0 && line[len - 1] == '\n')
+            {
+                line[len - 1] = '\0';
+                len--;
+            }
+        }
+    }
+    close(fd);
+    if (game->map.height == 0)
+        return (0);
+    scan_map_for_obj(game);
+
+    return (1);
+}
+
+
 
 void error_handel(char *av, t_map *data)
 {
@@ -176,6 +280,7 @@ int main(int ac, char **av)
     }
     error_handel(av[1], &data);
     ft_memset(&game, 0, sizeof(t_game));
+    ft_memset(&game.map, 0, sizeof(t_map));
     if (!load_map2(av[1], &game))
     {
         write(2, "Error loading map\n", 18);
